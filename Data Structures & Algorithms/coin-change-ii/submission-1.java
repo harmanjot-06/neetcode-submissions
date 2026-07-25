5f53ff91f2d44e476c79826402e48426e400dd0c
Harmanjot Singh <96612551+harmanjot-06@users.noreply.github.com>
2026-07-25 16:35:06 -0400

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        
+        if(amount==1)return 1;
+
+        int[][] dp = new int [coins.length + 1][amount+1];
+
+        for(int j = 1; j<dp[0].length; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=0; i<dp.length; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=1; i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+                int tempamount = j;
+
+                if(coins[i-1]<=tempamount){
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[coins.length][amount];
+
+    }
+}
