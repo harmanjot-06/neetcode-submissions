e403f48aa8dd496b3bfbdc0d33c1cfcbd5598914
Harmanjot Singh <96612551+harmanjot-06@users.noreply.github.com>
2026-08-08 15:22:51 -0400

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    List<List<Integer>> ans;
+    
+    public void helper(int[] nums, int i, int target, ArrayList<Integer> output){
+
+
+        if(target==0){
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+        if(i<0){
+            return;
+        }
+
+        if(nums[i]<=target){
+            helper(nums, i-1, target, output);
+            output.add(nums[i]);
+            helper(nums, i, target - nums[i], output);
+            output.remove(output.size()-1);
+            
+        }
+        else{
+            helper(nums, i-1, target, output);
+        }
+    }
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        
+        ans = new ArrayList<>();
+        Arrays.sort(nums);
+        helper(nums, nums.length - 1, target, new ArrayList<>());
+        return ans;
+    }
+}
