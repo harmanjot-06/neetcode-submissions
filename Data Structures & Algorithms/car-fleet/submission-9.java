d7d9b2225c9ae612050f3ff84c41cdefe4a79ae9
Harmanjot Singh <96612551+harmanjot-06@users.noreply.github.com>
2026-08-10 21:54:30 -0400

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    public boolean catchup(int pos1, int pos2, int speed1, int speed2, int target){
+
+        float relspeed = speed1 - speed2;
+        if(relspeed<=0)return false;
+
+        float reldistance = pos2 - pos1;
+
+        float time = reldistance/relspeed;
+
+        float finalpos = pos1 + time*speed1;
+
+        // System.out.println("finalpos " + finalpos + " target " + target);
+
+        if(finalpos<=target){
+
+            return true;
+        }
+        return false;
+    }
+
+    public int carFleet(int target, int[] position, int[] speed) {
+        
+        int n = position.length;
+        int[][] arr = new int[n][2];
+
+        for(int i=0; i<n; i++){
+            arr[i][0] = position[i];
+            arr[i][1] = speed[i];
+        }
+
+        Arrays.sort(arr, (int[] a, int[] b) -> Integer.compare(a[0], b[0]));
+
+        int fleece = 1;
+        int tocompare = n-1;
+        for(int i = n-2; i>=0; i--){
+
+            if(!catchup(arr[i][0], arr[tocompare][0], arr[i][1], arr[tocompare][1], target)){
+                fleece++;
+                tocompare = i;
+                // reduce = 0;
+            }
+            else{
+                // reduce++;
+            }
+        }
+
+        return fleece;
+    }
+}
